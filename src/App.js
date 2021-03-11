a3d787d418aa6153a751c1958caf778e208947cb
tscrosb <tscrosb@bu.edu>
2021-03-11 16:54:34 +0800

    Deploy React Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import marked from "marked";
+import defaultTextPath from "./startingText.md";
 
 function App() {
+  const [editorText, setEditorText] = useState("");
+
+  useEffect(() => {
+    marked.setOptions({ breaks: true, smartLists: true });
+    fetch(defaultTextPath)
+      .then(res => res.text())
+      .then(text => setEditorText(text));
+  }, []);
+
+  const handleText = event => {
+    setEditorText(event.target.value);
+  };
+
+  const reactMarkupParse = text => {
+    return { __html: marked(text) };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MarkDown Previewer</h1>
+      <div className="markdown-app">
+        <div className="editor-container">
+          <h2>Editor</h2>
+          <textarea
+            id="editor"
+            value={editorText}
+            onChange={handleText}
+          ></textarea>
+        </div>
+        <div className="preview-container">
+          <h2>Preview</h2>
+          <article
+            id="preview"
+            dangerouslySetInnerHTML={reactMarkupParse(editorText)}
+          ></article>
+        </div>
+      </div>
     </div>
   );
 }
